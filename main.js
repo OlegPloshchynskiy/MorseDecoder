0b98e68b2aeb8b32c021f8b8302654b79c9103a3
Olyezha <Oleg2pl27@gmail.com>
2023-01-20 19:25:16 +0200

    Init project

---
+++ main.js
@@ -0,0 +1,63 @@
+function decoderMorse(string) {
+
+    string = prompt("Write your message")
+
+    const morseCodeLetters = {
+        "-----":"0",
+        ".----":"1",
+        "..---":"2",
+        "...--":"3",
+        "....-":"4",
+        ".....":"5",
+        "-....":"6",
+        "--...":"7",
+        "---..":"8",
+        "----.":"9",
+        ".-":"a",
+        "-...":"b",
+        "-.-.":"c",
+        "-..":"d",
+        ".":"e",
+        "..-.":"f",
+        "--.":"g",
+        "....":"h",
+        "..":"i",
+        ".---":"j",
+        "-.-":"k",
+        ".-..":"l",
+        "--":"m",
+        "-.":"n",
+        "---":"o",
+        ".--.":"p",
+        "--.-":"q",
+        ".-.":"r",
+        "...":"s",
+        "-":"t",
+        "..-":"u",
+        "...-":"v",
+        ".--":"w",
+        "-..-":"x",
+        "-.--":"y",
+        "--..":"z",
+        "/":" ",
+        "-.-.--":"!",
+        ".-.-.-":".",
+        "--..--":","
+    }
+
+    const decoding = []
+
+    string.split("   ").map(function (word) {
+        word.split(" ").map(function (letter) {
+            decoding.push(morseCodeLetters[letter])
+        })
+        decoding.push(" ")
+    });
+
+    let result = decoding.join("");
+    console.log(result);
+
+}
+
+
+decoderMorse()
